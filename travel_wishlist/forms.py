e7bd474b9d4e12acf6751dfe9fc6a53a4cc1343d
Nelwell <nick.elwell206@gmail.com>
2021-04-07 03:23:41 -0500

    wishlist app - places, visiting places

---
+++ travel_wishlist/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Place
+
+
+class NewPlaceForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ('name', 'visited')  # only fields the user should be editing go here (ex. not PK)
+
+
+# class DateInput(forms.DateInput):
+#     input_type = 'date'
+#
+#
+# class TripReviewForm(forms.ModelForm):
+#     class Meta:
+#         model = Place
+#         fields = {'notes', 'date_visited', 'photo'}
+#         widgets = {
+#             'date_visited': DateInput()
+#         }
---
+++ wishlist/urls.py
@@ -0,0 +1,32 @@
+"""wishlist URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('travel_wishlist.urls'))  # make sure matches name of app name
+]
+#
+# """ If we are running this locally in development mode on our local machine, then add on these roots
+#  to static files, which will be based on the MEDIA_URL and the MEDIA_ROOT. >> So those things are
+#  based on where in your file system your current code is.
+# """
+# if settings.DEBUG:  # checks if is running locally using the development server
+#     # points to path on local comp defined in settings.py
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
